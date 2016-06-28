03a5bac491ce9f1a668e7fac1a0b4836d641ae4c
Carlos Orozco <orozco1582@gmail.com>
2016-06-27 22:25:42 -0500

    Add files via upload
    Hadoop problem 1

---
+++ tareaHadoop/FindPopularServer.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class FindPopularServer {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString(),"\n");
+      while (itr.hasMoreTokens()) {
+        
+        String line = itr.nextToken();
+        StringTokenizer tokensLine = new StringTokenizer(line);
+        
+        String server = "";
+        server = tokensLine.nextToken();
+
+        word.set(server);
+        context.write(word, one);
+      }
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    int max = 0;
+    Text maxWord = new Text();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+
+      for (IntWritable val : values) {
+        sum += val.get();
+      }      
+
+      if(sum>max){
+        max = sum;
+        maxWord.set(key);
+      }
+      
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+      context.write(maxWord, new IntWritable(max));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "Find Popular Server");
+    job.setJarByClass(FindPopularServer.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
