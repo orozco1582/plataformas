7b958eb13d7318d6b75a071f7cd503633371f204
Carlos Orozco <orozco1582@gmail.com>
2016-06-27 23:52:57 -0500

    Add files via upload

--- tareaHadoop/HadoopProblem2/mapper.py
+++ tareaHadoop/HadoopProblem2/mapper.py
@@ -2,13 +2,13 @@
 import sys
 
 for line in sys.stdin:
-	#line.replace("\t", ",")
+	line.replace("\t", ",")
 	
 	line = line.strip()
     unpacked = line.split(",")
-    #host, logname, time, method, url, response, bytes = line.split(",")
-    stadium, capacity, expanded, location, surface, turf, team, opened, weather, roof, elevation = line.split(",")
-    #if host == "edams.ksc.nasa.gov":
-    #	results = [url, "1"]
-    results = [turf, "1"]
+    host, logname, time, method, url, response, bytes = line.split(",")
+    #stadium, capacity, expanded, location, surface, turf, team, opened, weather, roof, elevation = line.split(",")
+    if host == "edams.ksc.nasa.gov":
+    	results = [url, "1"]
+    #results = [turf, "1"]
     print("\t".join(results))
